bef39859860309abc33a07622d73d5086c2f3e05
shengan <qjwgg.guguai@gmail.com>
2015-08-03 11:53:03 +0800

    add style to login page.

---
+++ mysite/blog/templatetags/myfilters.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from django import template
+
+register = template.Library()
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    return value.as_widget(attrs={'class': arg})
